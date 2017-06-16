0c13ad3f246245bed114c6c76e10860502b84fa2
Andrea Gentili <bluvertigo@gmail.com>
2017-06-16 13:53:26 +0200

    Update youtube-upload-cron.sh

--- youtube-upload-cron.sh
+++ youtube-upload-cron.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-#setup variables
-     x="/share/cam" #base dir to search
-     date=$(date +"%Y%m%d%H%M") #date used throughout script
-     fileprefix="yt_"
-     ytemail="youremail" #your youtube email account
-     ytpassword="password" #your youtube password
-     emailfrom="youremail(Your Name)" #your email address and from name
-     emailuser="youremail" #your gmail address
-     emailpassword="password" #your gmail password
-     emailto="youremail" #email to send message to
-     emailsubject="Security Camera Video Recorded" #email subject
      
-c_file=config_file_test
+c_file=simple.config.cfg
 
 unknown=`cat $c_file | grep -Evi "^(#.*|[a-z]*='[a-z0-9 ]*')$"`
 if [ -n "$unknown" ]; then
