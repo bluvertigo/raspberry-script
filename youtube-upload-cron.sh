78d68f4a52680cc3f005dfd7c7e15996bc898fb0
Andrea <bluvertigo@gmail.com>
2017-06-18 08:14:01 +0000

    Update files

--- youtube-upload-cron.sh
+++ youtube-upload-cron.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-     
+
 c_file=/home/pi/raspberry-script/config.cfg
 
-unknown=`cat $c_file | grep -Evi "^(#.*|[a-z]*='[a-z0-9 ]*')$"`
-if [ -n "$unknown" ]; then
- echo "Error in config file. Not allowed lines:"
- echo $unknown
- exit 1
-fi
+
+#unknown=`cat $c_file | grep -Evi "^(#.*|[a-z]*='[a-z0-9 ]*')$"`
+#if [ -n "$unknown" ]; then
+# echo "Error in config file. Not allowed lines:"
+# echo $unknown
+# exit 1
+#fi
 source $c_file
 echo "File Loaded"
 
+# make sure a copy directory is there otherwise make it
+if [ -d $COPY_DIR ]; then
+echo “You have a save directory”
+else
+mkdir $COPY_DIR
+fi
 
 #make list of files to work with that are at least x minutes old
      for i in $(find $x/ | grep -E 'avi|mov|mpeg|mp4'  | sort) ; do echo $i >> $x/$date.lst ; done
@@ -25,8 +32,15 @@ do
   #upload video
         if [ -a $filename ]
         then
-                echo $filename
-        fi
+                #result=$(youtube-upload --privacy="unlisted" --title="$fileprefix$date"  $filename)
+       		result="6dwqZw0j_jY"
+		echo -e "***\nFile: $result\n"
+		if [ echo "$result" | grep -q -E '([\w\'-]+)' ]
+		then
+    			echo "Valid date"
+		fi
+		#mv $filename $COPY_DIR
+	fi
 done
 
 
